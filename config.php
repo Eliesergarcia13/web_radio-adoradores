7fd2dd07f9ce4bd77593d192104bdf57d4346d1c
Eliesergarcia13 <155100307+Eliesergarcia13@users.noreply.github.com>
2024-08-08 23:09:54 -0300

    Add files via upload

---
+++ config.php
@@ -0,0 +1,57 @@
+<?php
+$pulse_dir = "admin";
+$page_title = "Web Rádio Adoradores - A estação do seu rádio";
+$page_desc = "Web Rádio Adoradores s - A estação do seu rádio";
+$page_words = "Rádio, online, web rádio, notícias, músicas, blog, eventos, vídeo, ao vivo.";
+$height = "270";
+$width = "270";
+$blog_url = "blog.php";
+$menu_home = "Home";
+$menu_sobre = "Sobre";
+$menu_noticias = "Notícias";
+$menu_mural = "Recados";
+$menu_programacao = "Programação";
+$menu_eventos = "Eventos";
+$menu_fotos = "Fotos";
+$menu_videos = "Vídeos";
+$menu_equipe = "Equipe";
+$menu_contato = "Contato";
+$youtube_url = "#";
+$analytics_id = "";
+$per_page = "3";
+$blog_comments = true;
+$blog_capcha = false;
+$date_format = "d/m/Y";
+$email_contact = "eliesergarcia13@gmail.com";
+$nub_whatsapp = "(55) 9 9679-3847";
+$facebook_url = "";
+$twitter_url = "https://twitter.com";
+$google_url = "https://plus.google.com";
+$linkedin_url = "#";
+$pinterest_url = "#";
+$instagram_url = "#";
+$flickr_url = "#";
+$pulse_lang = "PT_BR";
+$custom_fieldname1 = "";
+$custom_fieldname2 = "";
+$formcap = "1";
+$corsite = "green";
+
+$apk_url = "https://play.google.com/store/apps/details?id=seuid";
+$ios_url = "https://play.google.com/store/apps/details?id=seuid";
+$apk_select = "appoff.php";
+$ios_select = "appoff.php";
+
+
+$bapk_select = "off.php";
+$bios_select = "off.php";
+$bwin_select = "off.php";
+$bblack_select = "off.php";
+
+$bapk_url = "&lt;a href=&quot;https://radiosnet.com&quot; target=&quot;_blank&quot;&gt;&lt;img src=&quot;https://img.radios.com.br/divulgue/app-radiosnet-468x60-a.jpg&quot; alt=&quot;Para ouvir nossa rádio, baixe o aplicativo RadiosNet para celulares e tablets com Android ou iPhone/iPads.&quot; width=&quot;468&quot; height=&quot;60&quot; border=&quot;0&quot;&gt;&lt;/a&gt;";
+$bios_url = "https://play.google.com/store/apps/details?id=seuid";
+$bwin_url = "https://play.google.com/store/apps/details?id=seuid";
+$bblack_url = "https://play.google.com/store/apps/details?id=seuid";
+
+
+?>
